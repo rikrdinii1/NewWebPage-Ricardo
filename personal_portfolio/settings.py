f27f4f9248af4172481c3b3c42b4d5958a484bd6
rikrdinii1 <rikrdinii@hotmail.com>
2023-04-13 16:45:57 -0600

    se configura para servidor Vercel

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-4#nyn^cx+j1y)(vx3m2pe)dwy-wun^1i!(d4*s1by9*=)1!utn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
